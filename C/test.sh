a5b64698be6ba38842cc05959d57d08e01b3140d
Kevin Mazzarelli <kspam@sent.com>
2023-02-28 15:25:35 -0600

    Skeleton

---
+++ C/test.sh
@@ -0,0 +1,21 @@
+cd ./tests
+num_tests=$(ls -1 | grep '.test$' | wc -l | tr -d ' ')
+if [ ${num_tests} -ne 0 ]
+then
+	echo "TAP version 14"
+	echo "1..${num_tests}"
+	i=1
+	for test in *.test
+	do
+		if ./${test}
+		then
+			echo "ok ${i} - ${test}"
+		else
+			echo "not ok ${i} - ${test}"
+		fi
+	i=$((i+1))
+	done
+else
+	echo "Error: did you forget to run \`make'?" >&2
+	exit 1
+fi
