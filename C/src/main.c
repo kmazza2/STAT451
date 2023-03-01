028bf4c96a775aa2f04897fbf05279f01348f10d
Kevin Mazzarelli <kspam@sent.com>
2023-02-28 22:20:12 -0600

    Got dimensions of file

--- C/src/main.c
+++ C/src/main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include "csvinfo.h"
 int main(void) {
+	struct counts data_dim = data_shape("data/oilspills.dat");
+	printf("%lu\n", data_dim.fields);
+	printf("%lu\n", data_dim.rows);
 	printf("Success?\n");
 	return EXIT_SUCCESS;
 }
