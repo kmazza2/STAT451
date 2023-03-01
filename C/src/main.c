24651d3b4859403a3789d89a5897d0d71a3caf74
Kevin Mazzarelli <kspam@sent.com>
2023-02-28 22:46:02 -0600

    Using GSL now

--- C/src/main.c
+++ C/src/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "csvinfo.h"
+#include <gsl/gsl_block.h>
 int main(void) {
 	struct counts dc = data_counts("data/oilspills.dat");
 	printf("%lu\n", dc.fields);
 	printf("%lu\n", dc.rows);
+	gsl_block *raw_dat = gsl_block_alloc(dc.fields);
+	gsl_block_free(raw_dat);
 	return EXIT_SUCCESS;
 }
