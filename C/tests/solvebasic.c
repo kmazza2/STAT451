70a27c0bd65d256070423a9b06b1ba6ab1e5bbba
Kevin Mazzarelli <kspam@sent.com>
2023-03-02 13:48:31 -0600

    Read vector from file

--- C/tests/solvebasic.c
+++ C/tests/solvebasic.c
@@ -1,4 +1,5 @@
 #include "ucs_ftodm.h"
+#include "ucs_ftodv.h"
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,13 +8,12 @@
 
 int main(void) {
 	gsl_matrix *A = ucs_ftodm("data/A1.dat", false);
-	gsl_matrix *b_mat = ucs_ftodm("data/b1.dat", false);
-	gsl_vector_view b = gsl_matrix_column(b_mat, 0);
-	gsl_permutation *p = gsl_permutation_alloc(b.vector.size);
-	gsl_vector *x = gsl_vector_alloc(b.vector.size);
+	gsl_vector *b = ucs_ftodv("data/b1.dat", false);
+	gsl_permutation *p = gsl_permutation_alloc(b->size);
+	gsl_vector *x = gsl_vector_alloc(b->size);
 	int s;
 	gsl_linalg_LU_decomp(A, p, &s);
-	gsl_linalg_LU_solve(A, p, &b.vector, x);
+	gsl_linalg_LU_solve(A, p, b, x);
 	const double eps = 0.001;
 	if (
 			fabs(gsl_vector_get(x,0) - 0.4310) > eps ||
@@ -22,7 +22,7 @@ int main(void) {
 		return EXIT_FAILURE;
 	}
 	gsl_matrix_free(A);
-	gsl_matrix_free(b_mat);
+	gsl_vector_free(b);
 	gsl_permutation_free(p);
 	gsl_vector_free(x);
 	return EXIT_SUCCESS;
