c936f62e6a490211c2b9522b740e6d1b5d4cdc44
Kevin Mazzarelli <kspam@sent.com>
2023-03-02 22:57:54 -0600

    Better names

--- C/src/ucs_newton_fisher.c
+++ C/src/ucs_newton_fisher.c
@@ -19,25 +19,22 @@ struct ucs_iter_result ucs_newton_fisher(
 		.converged = false,
 		.iter = 0,
 	};
-	gsl_vector *cur_param = gsl_vector_alloc(param->size);
-	gsl_vector_memcpy(cur_param, param);
+	gsl_vector *next_param = gsl_vector_alloc(param->size);
+	gsl_vector_memcpy(next_param, param);
 	gsl_vector *first_deriv = gsl_vector_alloc(param->size);
 	gsl_matrix *second_deriv = gsl_matrix_alloc(param->size, param->size);
 	gsl_vector *update = gsl_vector_alloc(param->size);
-	while (1) {
-		if (result.iter == max_iter) {
-			break;
-		}
-		d1(cur_param, first_deriv);
-		d2(cur_param, second_deriv);
+	while (result.iter < max_iter) {
+		d1(next_param, first_deriv);
+		d2(next_param, second_deriv);
 		gsl_matrix_scale(second_deriv, -1);
 		ucs_solve(second_deriv, update, first_deriv);
-		gsl_vector_add(cur_param, update);
+		gsl_vector_add(next_param, update);
 		++result.iter;
 		if (
 				convergence_occurred(
 					param,
-					cur_param,
+					next_param,
 					epsabs,
 					epsrel
 				)
@@ -45,11 +42,11 @@ struct ucs_iter_result ucs_newton_fisher(
 			result.converged = true;
 			break;
 		}
-		gsl_vector_memcpy(param, cur_param);
+		gsl_vector_memcpy(param, next_param);
 	}
 	/* Return final value to user in param, even if no convergence */
-	gsl_vector_memcpy(param, cur_param);
-	gsl_vector_free(cur_param);
+	gsl_vector_memcpy(param, next_param);
+	gsl_vector_free(next_param);
 	gsl_vector_free(first_deriv);
 	gsl_vector_free(update);
 	gsl_matrix_free(second_deriv);
@@ -59,15 +56,15 @@ struct ucs_iter_result ucs_newton_fisher(
 
 bool convergence_occurred(
 		gsl_vector *param,
-		gsl_vector *cur_param,
+		gsl_vector *next_param,
 		double epsabs,
 		double epsrel
 ) {
 	double abs_diff;
 	for (int i = 0; i < param->size; ++i) {
 		double param_i = gsl_vector_get(param, i);
-		double cur_param_i = gsl_vector_get(cur_param, i);
-		abs_diff = fabs(cur_param_i - param_i);
+		double next_param_i = gsl_vector_get(next_param, i);
+		abs_diff = fabs(next_param_i - param_i);
 		if (
 				(abs_diff >= epsabs) ||
 				(abs_diff / (fabs(param_i) + DBL_MIN) >= epsrel)
