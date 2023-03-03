a56f0f071457bef9481f9051a78466cbc109ff6e
Kevin Mazzarelli <kspam@sent.com>
2023-03-02 22:18:41 -0600

    Newton appears to work

--- C/include/ucs_newton_fisher.h
+++ C/include/ucs_newton_fisher.h
@@ -1,12 +1,14 @@
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
+#include <stdbool.h>
 
-int ucs_newton_fisher(
-		gsl_vector *init_guess,
+bool ucs_newton_fisher(
+		gsl_vector *param,
 		size_t max_iter,
 		double epsabs,
 		double epsrel,
 		void (*d1)(gsl_vector *param, gsl_vector *val),
 		void (*d2)(gsl_vector *param, gsl_matrix *val),
-		const char *data
+		const char *data,
+		bool verbose
 );
---
+++ C/tests/newton_fisher_simple2.c
@@ -0,0 +1,29 @@
+#include "ucs_newton_fisher.h"
+#include <math.h>
+
+void d1(gsl_vector *param, gsl_vector *val) {
+	gsl_vector_set(val, 0, pow(gsl_vector_get(param, 0), 2) - 1);
+}
+
+void d2(gsl_vector *param, gsl_matrix *val) {
+	gsl_matrix_set(val, 0, 0, 2 * gsl_vector_get(param, 0));
+}
+
+int main(void) {
+	gsl_vector *init_guess = gsl_vector_alloc(1);
+	gsl_vector_set(init_guess, 0, 5);
+	bool converged = ucs_newton_fisher(
+		init_guess,
+		10000,
+		0.0001,
+		0.0001,
+		d1,
+		d2,
+		"data/A1.dat",
+		false
+	);
+	if (!converged) {
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
