102a1e0c35e0ac0af31df48fc635d43095cd39d4
Kevin Mazzarelli <kspam@sent.com>
2023-03-02 20:28:56 -0600

    Progress on simple newton test

--- C/tests/newton_fisher_dummy.c
+++ C/tests/newton_fisher_dummy.c
@@ -1,19 +1,17 @@
 #include "ucs_newton_fisher.h"
 
 void d1(gsl_vector *param, gsl_vector *val) {
-	gsl_vector_set(val, 0, 2 * gsl_vector_get(param, 1));
+        return;
 }
 
 void d2(gsl_vector *param, gsl_matrix *val) {
-	gsl_matrix_set(val, 0, 0, 2);
+        return;
 }
 
 int main(void) {
-	gsl_vector *init_guess = gsl_vector_alloc(1);
-	gsl_vector_set(init_guess, 0, 5);
 	int err = ucs_newton_fisher(
-		gsl_vector_alloc(1),
-		10000,
+		gsl_vector_alloc(0),
+		1000,
 		0.0001,
 		0.0001,
 		d1,
--- C/tests/sample/newton_fisher_dummy.h
+++
@@ -1,7 +0,0 @@
-void d1(gsl_vector *param, gsl_vector *val) {
-	return;
-}
-
-void d2(gsl_vector *param, gsl_matrix *val) {
-	return;
-}
