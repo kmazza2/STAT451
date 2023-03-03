09fc6ed810045c1b5faa8a4f197e7263aa9e2fd6
Kevin Mazzarelli <kspam@sent.com>
2023-03-02 23:24:24 -0600

    Comments

--- C/include/ucs_ftodm.h
+++ C/include/ucs_ftodm.h
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <gsl/gsl_matrix.h>
 
+/* reads file into matrix of doubles */
 /* This either succeeds or crashes the program, so
    it can be used as though it always succeeds.    */
 gsl_matrix *ucs_ftodm(char* path, bool header);
--- C/include/ucs_iter_result.h
+++ C/include/ucs_iter_result.h
@@ -1,5 +1,6 @@
 #include <gsl/gsl_vector.h>
 
+/* provides a return type for iterative methods like Newton-Raphson */
 struct ucs_iter_result {
 	bool converged;
 	size_t iter;
