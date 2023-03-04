8c85893a465d4b08023fe21202e37f18752cf1c0
Kevin Mazzarelli <kspam@sent.com>
2023-03-04 17:18:49 -0600

    Reorganize

--- C/test/poiss_newton_d2.c
+++ C/test/poiss_newton_d2.c
@@ -2,7 +2,7 @@
 #include <gsl/gsl_matrix.h>
 #include <math.h>
 #include <stdbool.h>
-#include "ucs_poiss_d.h"
+#include "ucs_poiss_newton.h"
 #include "ucs_ftodv.h"
 #include "ucs_ftodm.h"
 
@@ -10,16 +10,24 @@ int main(void)
 {
 	gsl_vector *param = ucs_ftodv("data/poiss_d1_param.dat", true);
 	gsl_matrix *data = ucs_ftodm("data/poiss_d1.dat", true);
-	gsl_vector *val = gsl_vector_alloc(param->size);
-	d1(param, data, val);
+	gsl_matrix *val = gsl_matrix_alloc(param->size, param->size);
+	d2(param, data, val);
 	if (
 			(fabs(
-				gsl_vector_get(val, 0) -
-				0.3702401814245953
+				gsl_matrix_get(val, 0, 0) -
+				-1.444953113663958
 			) > 0.01) ||
 			(fabs(
-				gsl_vector_get(val, 1) -
-				0.1951809091846201
+				gsl_matrix_get(val, 0, 1) -
+				-0.927506487901246
+			) > 0.01) ||
+			(fabs(
+				gsl_matrix_get(val, 1, 0) -
+				-0.927506487901246
+			) > 0.01) ||
+			(fabs(
+				gsl_matrix_get(val, 1, 1) -
+				-0.6102036501114272
 			) > 0.01)
 	) {
 		return EXIT_FAILURE;
