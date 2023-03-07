6bf2b639e7a5b86f8970f8fe26c694df98c4c8f5
Kevin Mazzarelli <kspam@sent.com>
2023-03-07 08:45:00 -0600

    Finished Newton

--- R/hw3/tests/newton.R
+++ R/hw3/tests/newton.R
@@ -1,6 +1,5 @@
 library(hw3)
-cat(getwd())
-d1 <- function (param, data) { return(matrix(c(-2*param[1], -2*param[2]))) }
-d2 <- function (param, data) { return(matrix(c(-2, 0, 0, -2), nrow=2)) }
+d1 <- function (param, data) { return(matrix(c(param[1]^(2)-1))) }
+d2 <- function (param, data) { return(matrix(c(2*param[1]))) }
 result <- newton("data/newton_data", "data/newton_param", d1, d2, 100, 0.01)
-stopifnot(result[1] == 1, result[4] == 0, result[5] == 0)
+stopifnot(result[1] == 1, abs(result[4][1] - (-1)) < 0.1)
