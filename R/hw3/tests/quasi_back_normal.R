95234c61a98a3e68f487111c170a1b815c842650
Kevin Mazzarelli <kspam@sent.com>
2023-03-07 12:29:16 -0600

    two parameter MLE tests for all

--- R/hw3/tests/quasi_back_normal.R
+++ R/hw3/tests/quasi_back_normal.R
@@ -16,9 +16,10 @@ I <- function (param, data) {
 		(length(data)/2)*param[2]^(-2)
 	), nrow=2))
 }
-result <- quasi("data/normal_data", "data/normal_param", d1, I, 100, 0.01)
+result <- quasi_back("data/normal_data", "data/normal_param", d1, I, 100, 0.01)
 stopifnot(
         result[1] == 1,
         abs(result[4] - (23)) < 0.1,
         abs(result[5] - (189.2)) < 0.1
 )
+
