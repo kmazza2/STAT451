a16519b3c62fa9f3ee12ff0e830d9be9354fe548
Kevin Mazzarelli <kspam@sent.com>
2023-03-07 05:50:51 -0600

    First pass at an R package

---
+++ R/hw3/R/ascent.R
@@ -0,0 +1,31 @@
+ascent <- function (data_path, param_path, max_iter, epsabs) {
+	data <- read.table(data_path, header=TRUE, sep=" ")
+	param <- as.matrix(read.table(param_path, header=TRUE, sep=" "))
+	converged <- FALSE
+	start_time <- proc.time()
+	for(iter in seq(max_iter)) {
+		derivative <- d1(param, data)
+		likelihood <- ll(param, data)
+		while(!(all(derivative == 0))) {
+			next_param <- param + derivative
+			next_likelihood <- ll(next_param, data)
+			if(next_likelihood > likelihood) {
+				break
+			}
+			derivative <- derivative / 2
+		}
+		if(norm(next_param - param, type="F") < epsabs) {
+			converged <- TRUE
+			break
+		}
+		param <- next_param
+	}
+	end_time <- proc.time()
+	cat(
+		"Converged: ", converged,
+		"\nIterations: ", iter,
+		"\nTime: ", (end_time - start_time)[3],
+		"s\nValue: ", next_param,
+		"\n"
+	)
+}
---
+++ R/hw3/R/quasi.R
@@ -0,0 +1,34 @@
+quasi <- function (data_path, param_path, max_iter, epsabs) {
+	data <- read.table(data_path, header=TRUE, sep=" ")
+	param <- as.matrix(read.table(param_path, header=TRUE, sep=" "))
+	converged <- FALSE
+	start_time <- proc.time()
+	M <- (-1) * I(param, data)
+	for(iter in seq(max_iter)) {
+		next_param <- param + solve((-1) * M, d1(param, data))
+		if(norm(next_param - param, type="F") < epsabs) {
+			converged <- TRUE
+			break
+		}
+		# otherwise, update M
+		y <- d1(next_param, data) - d1(param, data)
+		z <- next_param - param
+		v <- y - M %*% z
+		c_recip <- (t(v) %*% z)
+		# if c_recip is 0, don't change M
+		if(c_recip != 0) {
+			c <- 1 / c_recip
+			M <- M + c[1]  * (v %*% t(v))
+		}
+		# and set param to next_param
+		param <- next_param
+	}
+	end_time <- proc.time()
+	cat(
+		"Converged: ", converged,
+		"\nIterations: ", iter,
+		"\nTime: ", (end_time - start_time)[3],
+		"s\nValue: ", next_param,
+		"\n"
+	)
+}
