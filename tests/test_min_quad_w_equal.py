f02f5077d747082d0b045e26bcf66dc42422cb29
Kevin Mazzarelli <kevin@debian.attlocal.net>
2023-03-22 02:37:18 -0500

    Add test

--- tests/test_min_quad_w_equal.py
+++ tests/test_min_quad_w_equal.py
@@ -1,1 +1,14 @@
-import hw4.optim as optim
+import hw4.optim.optim as optim
+import numpy as np
+import unittest
+
+class TestOptim(unittest.TestCase):
+    def test_min_univar_quad_w_equal(self):
+        P = np.array([[2.0]])
+        q = np.array([[-8.0]])
+        r = np.array([[16.0]])
+        A = np.array([[3.0]])
+        b = np.array([[5.0]])
+        result = optim.min_quad_w_equal(P, q, r, A, b)[0,0]
+        expected = 5.0 / 3.0
+        self.assertEqual(expected, result)
