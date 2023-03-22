ac09c88f5608e76819e715f21afbec906b571322
Kevin Mazzarelli <kevin@debian.attlocal.net>
2023-03-22 03:11:29 -0500

    Tests for tests

--- tests/test_min_quad_w_equal.py
+++ tests/test_min_quad_w_equal.py
@@ -4,6 +4,18 @@ from scipy.linalg import norm
 import unittest
 
 
+def f(x):
+    return x[0, 0] ** 2 + x[1, 0] ** 2 + x[2, 0] ** 2
+
+
+def grad(x):
+    return np.array([[2.0 * x[0, 0], 2.0 * x[1, 0], 3.0 * x[2, 0] ** 2]]).T
+
+
+def hess(x):
+    return np.array([[2.0, 0.0, 0.0], [0.0, 2.0, 0.0], [0.0, 0.0, 6.0 * x[2, 0]]])
+
+
 class TestOptim(unittest.TestCase):
     def test_min_univar_quad_w_equal(self):
         P = np.array([[2.0]])
@@ -23,4 +35,20 @@ class TestOptim(unittest.TestCase):
         b = np.array([[1.0]])
         expected = np.array([[-0.5], [0.5]])
         result = optim.min_quad_w_equal(P, q, r, A, b)
+        self.assertTrue(norm(expected - result) < 0.0001)
+
+    def test_f(self):
+        x = np.array([[2, 3, 5]]).T
+        self.assertEqual(f(x), 38.0)
+
+    def test_grad(self):
+        x = np.array([[3, 5, 7]]).T
+        expected = np.array([[2.0 * 3.0, 2.0 * 5.0, 3.0 * 7.0**2]]).T
+        result = grad(x)
+        self.assertTrue(norm(expected - result) < 0.0001)
+
+    def test_hess(self):
+        x = np.array([[3, 5, 7]]).T
+        expected = np.array([[2.0, 0.0, 0.0], [0.0, 2.0, 0.0], [0.0, 0.0, 42.0]]).T
+        result = hess(x)
         self.assertTrue(norm(expected - result) < 0.0001)
