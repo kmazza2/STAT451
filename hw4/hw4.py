31e7541ff975fc5f2f70efba2e8ef1d9a8d9ac05
Kevin Mazzarelli <kevin@debian.attlocal.net>
2023-03-22 18:39:29 -0500

    rename tests

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -6,29 +6,8 @@ import optim.optim as optim
 p = mmread("data/p1_p.mm")
 pi = mmread("data/p1_pi.mm")
 x0 = mmread("data/p1_x0.mm")
-print(p)
-print(pi)
-print(x0)
+print(f"p:\n{p}")
+print(f"pi:\n{pi}")
+print(f"x0:\n{x0}")
 
 # Everything below here can be safely deleted
-
-
-def f(x):
-    return x[0, 0] ** 2 + x[1, 0] ** 2 + x[2, 0] ** 2
-
-
-def grad(x):
-    return np.array([[2.0 * x[0, 0], 2.0 * x[1, 0], 2.0 * x[2, 0]]]).T
-
-
-def hess(x):
-    return np.array([[2.0, 0.0, 0.0], [0.0, 2.0, 0.0], [0.0, 0.0, 2.0]])
-
-
-x0 = np.array([[1.3, 1.3, -1.3]]).T
-A = np.array([[-1.0, 1.0, 0.0], [1.0, 0.0, 1.0], [0.0, 0.0, 0.0]])
-b = np.array([[0.0, 0.0, 0.0]]).T
-expected = np.array([[0.0, 0.0, 0.0]]).T
-result = optim.newton_w_equal(f, grad, hess, x0, A, b, 0.00001, 100)
-print(expected)
-print(result)
