20d89629d037abfc05fa2ae60480dce0e6adc5f7
Kevin Mazzarelli <kmazza2@uic.edu>
2023-03-23 18:06:21 +0000

    Add data b, x0 for Problem 2

---
+++ data/p2_b.mm
@@ -0,0 +1,6 @@
+%%MatrixMarket matrix array real general
+% A 3x1 dense matrix; This is b in Problem 2
+3 1
+4.308065
+3.207879
+4.293116
---
+++ data/p2_x0.mm
@@ -0,0 +1,13 @@
+%%MatrixMarket matrix array real general
+% A 10x1 dense matrix; This is x0 in Problem 2
+10 1
+0.03277595
+0.94270937
+0.94773844
+0.90209127
+0.55226621
+0.22488620
+0.93071303
+0.24006548
+0.93490059
+0.44733386
