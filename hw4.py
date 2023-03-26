ae5c3bdc376c565b8a229db18ff9f7599e6c1a69
Kevin Mazzarelli <kevin@debian.attlocal.net>
2023-03-26 18:26:15 -0500

    Not quite there

--- hw4.py
+++ hw4.py
@@ -10,26 +10,25 @@ x0 = mmread("data/p1_x0.mm")
 
 result = portfolio.optimize(p, pi, x0, 1e-4, 100)
 print(
-    f"Problem 1(c)\nconverged: {result.converged}\niterations: {result.iterations}\nvalue:\n{result.x[0]}"
+    f"Problem 1(c)\n{result.x[0]}"
 )
 
 A = mmread("data/p2_A.mm")
 b = mmread("data/p2_b.mm")
 x0 = mmread("data/p2_x0.mm")
 
-print(A)  # DEBUG
 
 result = entropy.optimize(A, b, x0, 1e-4, 100)
 print(
-    f"Problem 2(a)\nconverged: {result.converged}\niterations: {result.iterations}\nvalue:\n{result.x[0]}"
+    f"Problem 2(a)\n{result.x[0]}"
 )
 
 m = 10
 p = 3
 
-x0 = np.ones((m + p, 1))
+x0 = np.ones((13,1))
 
-result = entropy_dual.optimize(m, p, A, b, x0, 1e-4, 100)
+result = entropy_dual.optimize(A, b, x0, 1e-4, 100)
 print(
-    f"Problem 2(c)\nconverged: {result.converged}\niterations: {result.iterations}\nvalue:\n{result.x[0]}"
+    f"Problem 2(c)\n{result.x[0]}"
 )
