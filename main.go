4de8460144aace19bbdb751c1234bc77607cd3df
Kevin Mazzarelli <kevin@localhost-live.attlocal.net>
2023-04-18 15:03:42 -0500

    Generate data

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	sm64 "github.com/kmazza2/urn/splitmix64"
-	x256xx "github.com/kmazza2/urn/xoshiro256xx"
-	u2f "github.com/kmazza2/urn/uint64tofloat64"
 	nrng "github.com/kmazza2/urn/normal"
+	sm64 "github.com/kmazza2/urn/splitmix64"
+	u2f "github.com/kmazza2/urn/uint64tofloat64"
+	x256xx "github.com/kmazza2/urn/xoshiro256xx"
 )
 
-const N = 100
+const N = 10000000
 const Seed = 0
 const Beta = 1.23456789
 
@@ -21,11 +21,31 @@ func main() {
 		seed_rng.Next())
 	unif_rng := u2f.NewUint64toFloat64(&src_rng)
 	normal_rng := nrng.NewNormalrng(&unif_rng, 0, 1)
-	x := [N]float64{}
-//	y := [N]float64{}
-	for i, _ := range x {
+	y := make([]float64, N)
+	x := make([]float64, N)
+	e := make([]float64, N)
+	for i := 0; i < N; i++ {
 		x[i] = unif_rng.Next()
 	}
-	fmt.Println(x)
-	fmt.Println(normal_rng.Next())
+	for i := 0; i < N; i++ {
+		e[i] = normal_rng.Next()
+	}
+	for i := 0; i < N; i++ {
+		y[i] = Beta*x[i] + e[i]
+	}
+	fmt.Println(mean(y))
+	fmt.Println(mean(x))
+	fmt.Println(mean(e))
+}
+
+func sum(s []float64) float64 {
+	result := 0.
+	for _, v := range s {
+		result += v
+	}
+	return result
+}
+
+func mean(s []float64) float64 {
+	return sum(s) / float64(len(s))
 }
