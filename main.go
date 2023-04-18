67728ccc66a64d07ee07529664d426a0a0d55cb1
Kevin Mazzarelli <kevin@localhost-live.attlocal.net>
2023-04-18 15:19:28 -0500

    Construct confidence interval analytically

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	sm64 "github.com/kmazza2/urn/splitmix64"
 	u2f "github.com/kmazza2/urn/uint64tofloat64"
 	x256xx "github.com/kmazza2/urn/xoshiro256xx"
+	"math"
 )
 
 const N = 10000000
 const Seed = 0
 const Beta = 1.23456789
+const InvCdf = -1.959963984540054
 
 func main() {
 	seed_rng := sm64.NewSplitMix64(Seed)
@@ -36,6 +38,10 @@ func main() {
 	fmt.Println(mean(y))
 	fmt.Println(mean(x))
 	fmt.Println(mean(e))
+	fmt.Println("lower:")
+	fmt.Println(beta1_hat(x, y) - T(x))
+	fmt.Println("upper:")
+	fmt.Println(beta1_hat(x, y) + T(x))
 }
 
 func sum(s []float64) float64 {
@@ -48,4 +54,22 @@ func sum(s []float64) float64 {
 
 func mean(s []float64) float64 {
 	return sum(s) / float64(len(s))
+}
+
+func beta1_hat(x []float64, y []float64) float64 {
+	num := 0.
+	denom := 0.
+	for i := 0; i < len(x); i++ {
+		num += x[i] * y[i]
+		denom += x[i] * x[i]
+	}
+	return num / denom
+}
+
+func T(x []float64) float64 {
+	sum := 0.
+	for i := 0; i < len(x); i++ {
+		sum += x[i] * x[i]
+	}
+	return -1. * InvCdf / math.Sqrt(sum)
 }
