6b235a51ffbaa9427c9e9d9ee3d3a70ddaa1bace
Kevin Mazzarelli <kevin@localhost-live.attlocal.net>
2023-04-18 14:33:06 -0500

    Create uniform RNG

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kmazza2/STAT451/HW5
 
 go 1.20
+
+require github.com/kmazza2/urn v0.0.0-20230413160613-b520f08552a1
--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	sm64 "github.com/kmazza2/urn/splitmix64"
+	x256xx "github.com/kmazza2/urn/xoshiro256xx"
+	u2f "github.com/kmazza2/urn/uint64tofloat64"
 )
 
+const N = 100
+const Seed = 0
+
 func main() {
-	fmt.Println("Hello from HW5. Time to bootstrap.")
+	seed_rng := sm64.NewSplitMix64(Seed)
+	src_rng := x256xx.NewXoshiro256xx(
+		seed_rng.Next(),
+		seed_rng.Next(),
+		seed_rng.Next(),
+		seed_rng.Next())
+	unif_rng := u2f.NewUint64toFloat64(&src_rng)
+	fmt.Println(unif_rng.Next())
 }
