572813eef363067bb265807783e907762b59c296
Kevin Mazzarelli <kevin@localhost-live.attlocal.net>
2023-04-18 14:46:51 -0500

    Fixed Normalrng API

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/kmazza2/STAT451/HW5
 
 go 1.20
 
-require github.com/kmazza2/urn v0.0.0-20230413160613-b520f08552a1
+require github.com/kmazza2/urn v0.0.0-20230418194506-d681d88382ab
--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	sm64 "github.com/kmazza2/urn/splitmix64"
 	x256xx "github.com/kmazza2/urn/xoshiro256xx"
 	u2f "github.com/kmazza2/urn/uint64tofloat64"
+	nrng "github.com/kmazza2/urn/normal"
 )
 
 const N = 100
 const Seed = 0
+const Beta = 1.23456789
 
 func main() {
 	seed_rng := sm64.NewSplitMix64(Seed)
@@ -18,5 +20,12 @@ func main() {
 		seed_rng.Next(),
 		seed_rng.Next())
 	unif_rng := u2f.NewUint64toFloat64(&src_rng)
-	fmt.Println(unif_rng.Next())
+	normal_rng := nrng.NewNormalrng(&unif_rng, 0, 1)
+	x := [N]float64{}
+//	y := [N]float64{}
+	for i, _ := range x {
+		x[i] = unif_rng.Next()
+	}
+	fmt.Println(x)
+	fmt.Println(normal_rng.Next())
 }
