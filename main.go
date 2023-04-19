6142ff0a80e7ee232b7efba772c4f5d1f569d4b8
Kevin Mazzarelli <kspam@sent.com>
2023-04-19 11:40:20 -0500

    Implement resample function

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	drng "github.com/kmazza2/urn/dunif"
+	f64rng "github.com/kmazza2/urn/float64rng"
 	nrng "github.com/kmazza2/urn/normal"
 	sm64 "github.com/kmazza2/urn/splitmix64"
 	u2f "github.com/kmazza2/urn/uint64tofloat64"
@@ -9,7 +11,7 @@ import (
 	"math"
 )
 
-const Trials = 10000000
+const Trials = 1000000
 const Samples = 30
 const Seed = 0
 const Beta = 2.
@@ -38,11 +40,14 @@ func main() {
 		for j := 0; j < Samples; j++ {
 			y[j] = Beta*x[j] + e[j]
 		}
-		lower := beta1_hat(x, y) - T(x)
-		upper := beta1_hat(x, y) + T(x)
+		// The dataset has now been generated.
+		// Calculate the confidence interval analytically:
+		lower := beta1_hat(x, y) - t(x)
+		upper := beta1_hat(x, y) + t(x)
 		if lower <= Beta && Beta <= upper {
 			ci_contains_param += 1.
 		}
+		// Calculate the confidence interval using bootstrap:
 	}
 	fmt.Println(ci_contains_param / Trials)
 }
@@ -69,10 +74,19 @@ func beta1_hat(x []float64, y []float64) float64 {
 	return num / denom
 }
 
-func T(x []float64) float64 {
+func t(x []float64) float64 {
 	sum := 0.
 	for i := 0; i < len(x); i++ {
 		sum += x[i] * x[i]
 	}
 	return -1. * InvCdf / math.Sqrt(sum)
+}
+
+func resample(data []float64, src_rng f64rng.Float64rng) []float64 {
+	dunif_rng := drng.NewDunifrng(src_rng, Samples)
+	result := make([]float64, len(data))
+	for i := 0; i < len(data); i++ {
+		result[i] = data[dunif_rng.Next()-1]
+	}
+	return result
 }
